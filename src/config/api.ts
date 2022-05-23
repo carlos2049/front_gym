baceb0384933f7ffc48ef0b8efcb31092e4afdd1
carlos2049 <carlos.20499@gmail.com>
2022-05-22 23:24:18 -0400

    add Login view and add new route

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  headers: { 'Authorization': `Bearer ${localStorage.getItem('React_token')}` },
+  headers: { 'Authorization': `Bearer ${localStorage.getItem('accessToken')}` },
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes as Switch, } from 'react-router-dom'
 // import Profile from '../views/Profile';
 // import DefaultLayout from '../layouts/DefaultLayout';
 import Users from '../views/Users';
@@ -7,21 +7,34 @@ import Profiles from '../views/Profiles';
 import Permissions from '../views/Permissions';
 import Plans from '../views/Plan';
 import Subplans from '../views/Subplans';
+import Login from '../views/Login';
 const lazyLoad = (component: any) => lazy(async () => component);
 const DefaultLayout = lazyLoad(import('../layouts/DefaultLayout'))
 const Routes = () => {
+  const bol = false
   return (
     <Suspense fallback={null} >
       <BrowserRouter>
-        <Switch>
-          <Route path="/" element={<DefaultLayout />}>
-            <Route path="users" element={<Users />} />
-            <Route path="permissions" element={<Permissions />} />
-            <Route path="profiles" element={<Profiles />} />
-            <Route path="plans" element={<Plans />} />
-            <Route path="subplans" element={<Subplans />} />
-          </Route>
-        </Switch>
+        {
+          bol ?
+            <Switch>
+              <Route path="/" element={<DefaultLayout />}>
+                <Route path="users" element={<Users />} />
+                <Route path="permissions" element={<Permissions />} />
+                <Route path="profiles" element={<Profiles />} />
+                <Route path="plans" element={<Plans />} />
+                <Route path="subplans" element={<Subplans />} />
+              </Route>
+            </Switch> :
+            <Switch>
+              <Route
+                path="*"
+                element={<Navigate to="/login" replace />}
+              />
+              <Route path='/login' element={<Login />} />
+
+            </Switch>
+        }
       </BrowserRouter>
     </Suspense >
   )
